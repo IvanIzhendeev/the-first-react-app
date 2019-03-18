2dedd05128a5682f9ffb50dbeddcd2086f8598b6
Иван Ижендеев <ivanizendeev@MacBook-Pro-Ivan-2.local>
2019-03-18 18:36:15 +0300

    The API is configured. Add bootstrap

--- src/App.js
+++ src/App.js
@@ -8,21 +8,47 @@ const OPEN_WEATHER_API_KEY = "e1c6d5eab1f6c4d3be688c849d4cdf5d";
 class App extends React.Component{
 
   state = {
-      temp: undefined,
-      temp_max: undefined,
-      temp_min: undefined,
-      sunrise: undefined,
-      sunset: undefined,
-      humidity: undefined,
-      pressure: undefined
+    city: undefined,
+    temp: undefined,
+    temp_max: undefined,
+    temp_min: undefined,
+    wind_speed: undefined,
+    wind_deg: undefined,
+    humidity: undefined,
+    pressure: undefined,
+    error: undefined
   }
 
   gettingWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
+    if(city){
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}&units=metric`);
+      const data = await api_url.json();
+      this.setState({
+        city: data.name,
+        temp: data.main.temp,
+        temp_max: data.main.temp_max,
+        temp_min: data.main.temp_min,
+        wind_speed: data.wind.speed,
+        wind_deg: data.wind.deg,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        error: undefined
+      });
+    } else{
+      this.setState({
+        city: undefined,
+        temp: undefined,
+        temp_max: undefined,
+        temp_min: undefined,
+        wind_speed: undefined,
+        wind_deg: undefined,
+        humidity: undefined,
+        pressure: undefined,
+        error: "Введите название города"
+      });
+    }
   }
 
   render(){
@@ -30,7 +56,17 @@ class App extends React.Component{
       <div>
         <Info />
         <Form weatherMethod = {this.gettingWeather} />
-        <Weather />
+        <Weather 
+          city = {this.state.city}
+          temp = {this.state.temp}
+          temp_max = {this.state.temp_max}
+          temp_min = {this.state.temp_min}
+          wind_speed = {this.state.wind_speed}
+          wind_deg = {this.state.wind_deg}
+          humidity = {this.state.humidity}
+          pressure = {this.state.pressure}
+          error = {this.state.error}
+        />
       </div> 
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,13 +1,21 @@
 import React from "react";
- 
-class Weather extends React.Component{
-  render(){
-    return (
+
+const Weather = props => (
+  <div>
+    {props.city &&
       <div>
-        Компонент с погодной информацией.
-      </div> 
-    );
-  }
-}
+        <p>Город:{props.city}</p>
+        <p>Температура:{props.temp}</p>
+        <p>Максимальная температура:{props.temp_max}</p>
+        <p>Минимальная температура:{props.temp_min}</p>
+        <p>Скорость ветра:{props.wind_speed}</p>
+        <p>Направление ветра:{props.wind_deg}</p>
+        <p>Влажность:{props.humidity}</p>
+        <p>Давление:{props.pressure}</p>
+      </div>
+    }
+    <p>{props.error}</p>
+  </div> 
+)
 
 export default Weather;
